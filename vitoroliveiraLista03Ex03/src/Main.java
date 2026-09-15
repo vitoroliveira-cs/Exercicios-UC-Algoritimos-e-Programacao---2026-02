91fdcbfb6f96bfe5013d28d8e07194f4899a2fdc
Vitor Hugo <vitor.olveira.cs@gmail.com>
2026-09-15 19:38:27 -0300

    Add files via upload

---
+++ vitoroliveiraLista03Ex03/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 20; i+=2) {
+            System.out.println("Valor é: " + i);
+
+        }
+    }
+}
